31eed93ed4a7fc506a527f86578261c8e807caf6
Az <achmad.azman@gmail.com>
2022-08-31 22:40:53 +0800

    install plugin

---
+++ wp-content/mu-plugins/acf.php
@@ -0,0 +1,16 @@
+<?php
+/*
+Plugin Name: Advanced Custom Fields PRO
+Plugin URI: https://www.advancedcustomfields.com/
+Description: Customize WordPress with powerful, professional and intuitive fields.
+Version: 5.8.0-RC2
+Author: Elliot Condon
+Author URI: http://www.elliotcondon.com/
+Copyright: Elliot Condon
+Text Domain: acf
+Domain Path: /lang
+*/
+
+if( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+include 'advanced-custom-fields-pro/acf.php';
